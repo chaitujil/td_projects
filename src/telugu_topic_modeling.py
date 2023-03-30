62b835639daf986027bcc66c2777bad531676962
Krishna Yalamanchili <chaitu.jil@gmail.com>
2023-03-29 17:31:02 -0700

    initial commit

--- src/telugu_topic_modeling.py
+++ src/telugu_topic_modeling.py
@@ -1,6 +1,6 @@
 from sentence_transformers import SentenceTransformer
 sentences = ["This is an example sentence", "Each sentence is converted"]
 
-model = SentenceTransformer('TeluguSBERT-STS')
+model = SentenceTransformer('l3cube-pune/telugu-sentence-similarity-sbert')
 embeddings = model.encode(sentences)
 print(embeddings)
